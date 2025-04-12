b4183b42b26c5e73c6afdb50e1474bfcbdcc2d7e
Vasilije Trpchev <vasilije_trpcev@yahoo.com>
2025-04-12 12:58:11 +0200

    banner done

--- Kotur/src/main.tsx
+++ Kotur/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // Font Awesome
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -11,7 +12,7 @@ import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
--- Kotur/src/pages/Menu/MenuPage.tsx
+++ Kotur/src/pages/Menu/MenuPage.tsx
@@ -1,6 +1,48 @@
 import styles from "./MenuPage.module.css";
 const MenuPage = () => {
-  return <div>MenuPage</div>;
+  return (
+    <div className={`${styles.menuBanner}`}>
+      <div className="row justify-content-center align-items-center  h-100 w-100">
+        <div className="col-lg-5 col-12 text-white d-flex flex-column justify-content-center align-items-start pe-0 ps-lg-5 px-4 mb-4 mb-lg-0">
+          <h2 className="pb-4 text-center ps-5  ">МЕНИ</h2>
+          <p className="w-50 ps-5 ">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+            nesciunt omnis deleniti odit ducimus. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Voluptas, id.
+          </p>
+        </div>
+        <div className="col-lg-7  col-12 d-flex justify-content-around align-items-center  ">
+          <a
+            href="https://www.instagram.com/koturkafeifilm/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.card}`}
+          >
+            <img src="./images/menu-page/img1.jpg" alt="Menu item 1" />
+            <i className={`${styles.iconLink} bi bi-instagram`}></i>
+          </a>
+          <a
+            href="https://www.instagram.com/koturkafeifilm/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.card}`}
+          >
+            <img src="./images/menu-page/img2.jpg" alt="Menu item 2" />
+            <i className={`${styles.iconLink} bi bi-instagram`}></i>
+          </a>
+          <a
+            href="https://www.instagram.com/koturkafeifilm/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${styles.card}`}
+          >
+            <img src="./images/menu-page/img3.jpg" alt="Menu item 3" />
+            <i className={`${styles.iconLink} bi bi-instagram`}></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MenuPage;
