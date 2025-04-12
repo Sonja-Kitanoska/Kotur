896eaf67f704550f27d00e5558fb5cc33d10566f
Sonja Kitanoska <avramoskamkd@gmail.com>
2025-04-12 09:34:58 +0200

    AboutUsHero

--- Kotur/src/pages/AboutUs/AboutUsPage.tsx
+++ Kotur/src/pages/AboutUs/AboutUsPage.tsx
@@ -4,6 +4,15 @@ import styles from "./AboutUsPage.module.css";
 const AboutUsPage = () => {
 	return (
 		<div>
+			<div className={styles.aboutUsHero}>
+				<div className={styles.circularText}>
+					<img
+						src="/images/AboutUsImages/istorija-kotur.png"
+						alt="Istorija na Kotur"
+						className="w-100"
+					/>
+				</div>
+			</div>
 			<Testimonials />
 		</div>
 	);
