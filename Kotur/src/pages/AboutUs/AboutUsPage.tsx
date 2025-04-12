f746094d40c52e6557fba50b9866056a60995053
Sonja Kitanoska <avramoskamkd@gmail.com>
2025-04-12 08:45:21 +0200

    Testimonials component added to About us page

--- Kotur/src/pages/AboutUs/AboutUsPage.tsx
+++ Kotur/src/pages/AboutUs/AboutUsPage.tsx
@@ -1,7 +1,12 @@
+import Testimonials from "../../components/Testimonials";
 import styles from "./AboutUsPage.module.css";
 
 const AboutUsPage = () => {
-  return <div>AboutUs</div>;
+	return (
+		<div>
+			<Testimonials />
+		</div>
+	);
 };
 
 export default AboutUsPage;
