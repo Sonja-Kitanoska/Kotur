77f87163e730aabbb253f6ab4e00f71f222c0a1f
Sonja Kitanoska <avramoskamkd@gmail.com>
2025-04-13 10:27:17 +0200

    Problem with not scrolling to top when navigating fixed

--- Kotur/src/App.tsx
+++ Kotur/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import AboutUsPage from "./pages/AboutUs/AboutUsPage";
@@ -6,19 +6,32 @@ import MenuPage from "./pages/Menu/MenuPage";
 import EventsPage from "./pages/Events/EventsPage";
 import GalleryPage from "./pages/Gallery/GalleryPage";
 import Layout from "./pages/Layout/Layout";
+import { useEffect } from "react";
 
 function App() {
+	const Wrapper = ({ children }: { children: React.ReactNode }) => {
+		const location = useLocation();
+
+		useEffect(() => {
+			window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+		}, [location.pathname]);
+
+		return <>{children}</>;
+	};
+
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route path="/" element={<LandingPage />} />
-					<Route path="/about-us" element={<AboutUsPage />} />
-					<Route path="/menu" element={<MenuPage />} />
-					<Route path="/events" element={<EventsPage />} />
-					<Route path="/gallery" element={<GalleryPage />} />
-				</Route>
-			</Routes>
+			<Wrapper>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route path="/" element={<LandingPage />} />
+						<Route path="/about-us" element={<AboutUsPage />} />
+						<Route path="/menu" element={<MenuPage />} />
+						<Route path="/events" element={<EventsPage />} />
+						<Route path="/gallery" element={<GalleryPage />} />
+					</Route>
+				</Routes>
+			</Wrapper>
 		</BrowserRouter>
 	);
 }
