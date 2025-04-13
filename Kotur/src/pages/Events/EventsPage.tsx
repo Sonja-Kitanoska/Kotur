ac4cefe604f3cedf3be90aa9e6e6c6c85f59e615
Vasilije Trpchev <vasilije_trpcev@yahoo.com>
2025-04-13 11:59:19 +0200

    pointer added on horizontal scroll

--- Kotur/src/pages/Events/EventsPage.tsx
+++ Kotur/src/pages/Events/EventsPage.tsx
@@ -2,20 +2,20 @@ import EventsCalendar from "../../components/EventsCalendar";
 import FutureEventsCard from "../LandingPage/FutureEventsCard";
 import styles from "./EventsPage.module.css";
 const EventsPage = () => {
-	return (
-		<>
-			<div className="bg-black">
-				<div className={styles.eventsHeroImage}></div>
-			</div>
+  return (
+    <>
+      <div className="bg-black">
+        <div className={styles.eventsHeroImage}></div>
+      </div>
 
-			<EventsCalendar />
-			<div className={`container-fluid my-5`}>
-				<div className="row text-white ">
-					<FutureEventsCard />
-				</div>
-			</div>
-		</>
-	);
+      <EventsCalendar />
+      <div className={`container-fluid my-5`}>
+        <div className="row text-white ">
+          <FutureEventsCard />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default EventsPage;
